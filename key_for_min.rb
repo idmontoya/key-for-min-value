59e2d91d9bfbbad7c09a7f4b8ce3df2d315c4f19
Isaiah Montoya <idmontoya@yahoo.com>
2021-03-05 23:26:59 -0900

    commit tree 509f9b1dbb60585748313b5b61bb7b54cf3404e9

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+    lowest_value = nil
+    lowest_key = nil
+    name_hash.each do |k, v|
+        if lowest_value == nil
+            lowest_value = v
+            lowest_key = k
+        elsif lowest_value > v
+            lowest_value = v
+            lowest_key = k
+        end
+    end
+    
+    lowest_key
 
 end
